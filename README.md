8bb65a8043fbde9a406cf73e4b8d582ea560fc01
dogankocadayilar <dogankocadayilar@gmail.com>
2023-01-25 11:17:42 +0300

    Updated REAMDE.md

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 This is a solution to the [Pricing component with toggle challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/pricing-component-with-toggle-8vPwRMIC). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
-**Currently working in chrome browsers because of importing a JSON file**
-
 ### The challenge
 
 Users should be able to:
--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-import data from "./data.json" assert { type: "json" };
 import Card from "./Card.js";
+
+const data = [
+  {
+    title: "Basic",
+    monthlyPricing: "19.99",
+    annualyPricing: "199.99",
+    storageSize: "500 GB",
+    allowedUsers: "2",
+    sendSize: "3",
+  },
+  {
+    title: "Professional",
+    monthlyPricing: "24.99",
+    annualyPricing: "249.99",
+    storageSize: "1 TB",
+    allowedUsers: "5",
+    sendSize: "10",
+  },
+  {
+    title: "Master",
+    monthlyPricing: "39.99",
+    annualyPricing: "399.99",
+    storageSize: "2 Tb",
+    allowedUsers: "10",
+    sendSize: "20",
+  },
+];
 
 const toggler = document.getElementById("toggler");
 const cardContainer = document.getElementById("cardContainer");
